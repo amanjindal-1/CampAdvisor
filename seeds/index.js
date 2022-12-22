a2a83e9c854347185af6ae4f831454d1a4621359
Aman Jindal <amanjindal231@gmail.com>
2022-12-22 16:33:01 +0530

    Initial Commit

---
+++ seeds/index.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://127.0.0.1:27017/camp-advisor', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+        console.log("Database connected")
+    })
+    .catch(err => {
+        console.log("OH NO DATABASE CONNECTION ERROR!!!!")
+        console.log(err)
+    });
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa nobis, exercitationem id eligendi et minima odit. Fuga veniam pariatur repudiandae provident ipsam ad earum corrupti ab voluptate deserunt, illum ut!',
+            price: price
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+});
